3d7e60c82ae93642a5d960f97f1366775b18ce07
itsjenm <jmartinez414@gmail.com>
2023-02-06 17:38:37 -0600

    task3 completion

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,26 +1,64 @@
-import Notiflix from 'notiflix';
+import * as Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delay = document.querySelector('[name="delay"]');
+const amount = document.querySelector('[name="amount"]');
+const increment = document.querySelector('[name="step"]');
+const start = document.querySelector('[type="submit"]');
 
 
 
-// function createPromise(position, delay) {
-//   const position = document.getElementsByName('amount');
-//   const shouldResolve = Math.random() > 0.3;
-//   setTimeout(() => {
-//     if (shouldResolve) {
-//       // Fulfill
-//     } else {
-//       // Reject
-//     }
-//   }, position);
-// }
+//Write a script that, when submitting the form, calls the createPromise(position, delay) function as many times as you entered in the amount field\
+
+form.addEventListener("submit", submittedPromises)
 
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+  function submittedPromises(event) {
+    // get values from form inputs and save it to object every time event is called 
+      event.preventDefault();
+
+     
+      let firstDelay = parseInt(delay.value);
+      const incrementDelay = parseInt(increment.value);
+      const amountOfPromise = parseInt(amount.value);
+  
+
+      console.log(firstDelay)
+      console.log(incrementDelay)
+      console.log(amountOfPromise)
+
+      let position; //sequence number of promise
+    
+      
+      for (position = 1; position <= amountOfPromise; position++) {
+        createPromise(position, firstDelay)
+          .then(({ position, delay }) => {
+            Notiflix.Notify.success(
+              `✅ Fulfilled promise ${position} in ${delay}ms`
+            );
+            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+          })
+          .catch(({ position, delay }) => {
+            Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+            console.log(`❌ Rejected promise ${position} in ${delay}ms`)
+          });
+    
+          firstDelay += incrementDelay
+        }
+      }
+
+
+
+
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
