05b3d1a836a426dfd0e0e382791d0f5617d8ccc8
itsjenm <77757004+itsjenm@users.noreply.github.com>
2023-02-15 06:19:35 +0000

    Deploying to gh-pages from @ itsjenm/goit-js-hw-09@def1dc751373178f4fb3ee3afcf6d1babf922b89 🚀

---
+++ 01-color-switcher.1b294611.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;const a=()=>(d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),d);e.disabled=!0,t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,a()})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.1b294611.js.map
