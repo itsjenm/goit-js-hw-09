def1dc751373178f4fb3ee3afcf6d1babf922b89
itsjenm <jmartinez414@gmail.com>
2023-02-15 00:18:59 -0600

    task 1 button updates

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,30 @@
 const startButton = document.querySelector('button[data-start]');
-const stopButton = document.querySelector('button[data-stop]');
+const stopButton = document.querySelector('button[data-stop]')
 let timerId = null;
 
 const changeColor = () => {
   timerId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
   }, 1000);
+  return timerId
 };
 
-startButton.addEventListener('click', changeColor);
+stopButton.disabled = true;
+
+startButton.addEventListener('click', () => {
+  stopButton.disabled = false;
+  startButton.disabled = true;
+  changeColor()
+});
 
 stopButton.addEventListener('click', () => {
+  startButton.disabled = false
   clearInterval(timerId);
 });
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
